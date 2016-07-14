1a0065acd29b26c917df0f8e6ca4b9fd00a38686
zhangcc <zhangcc@DESKTOP-9UFUGVO>
2016-07-14 22:40:35 +0800

    修改错误代码

--- src/cn/yicha/adchannel/route/ControllerRoutes.java
+++ src/cn/yicha/adchannel/route/ControllerRoutes.java
@@ -1,9 +1,7 @@
 package cn.yicha.adchannel.route;
 
-import cn.yicha.adchannel.controller.ChannelController;
 import cn.yicha.adchannel.controller.LoginController;
 import cn.yicha.adchannel.controller.IndexController;
-import cn.yicha.adchannel.controller.ProductController;
 
 import com.jfinal.config.Routes;
 
@@ -19,8 +17,6 @@ public class ControllerRoutes extends Routes {
 		// TODO Auto-generated method stub
 		add("/", LoginController.class);
 		add("/index", IndexController.class);
-		add("/channel", ChannelController.class);
-		add("/product", ProductController.class);
 	}
 
 }
